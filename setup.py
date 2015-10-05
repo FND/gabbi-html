1242f4818ed8ac67563fc699b8da790e4330d616
FND <fnd@localhost.localdomain>
2015-10-05 20:40:31 +0200

    added missing metadata

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 META = {
     "name": "gabbi-html",
+    "version": "0.1.0", # TODO: move into package
+    "url": "https://github.com/FND/gabbi-html",
+    "author": "FND",
     "install_requires": ["gabbi", "lxml", "cssselect"],
     "extras_require": {
         "linting": ["pep8"]
@@ -8,4 +11,5 @@ META = {
 
 
 if __name__ == "__main__":
+    from setuptools import setup
     setup(**META)
