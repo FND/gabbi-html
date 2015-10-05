a7113c84b87daa1d39e01b79406f041c753dab18
FND <fnd@localhost.localdomain>
2015-10-05 21:26:00 +0200

    fixed distribution info

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 META = {
     "name": "gabbi-html",
-    "version": "0.1.0", # TODO: move into package
+    "version": "0.1.1", # TODO: move into package
     "url": "https://github.com/FND/gabbi-html",
     "author": "FND",
+    "py_modules": ["gabbi_html"],
     "install_requires": ["gabbi", "lxml", "cssselect"],
     "extras_require": {
         "linting": ["pep8"]
